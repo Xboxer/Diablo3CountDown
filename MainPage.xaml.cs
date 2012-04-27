7b8abbec4a51d14807b1caa91366c5b1fa983a93
Christoph Kaliske <christoph@kaliske.eu>
2012-04-27 13:30:36 +0200

    Design änderungen + Meldung nach Release

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -40,9 +40,21 @@ namespace Diablo3_Countdown
         {
             var launch = new DateTime(2012, 05, 15);
             var timeLeft = launch - DateTime.Now;
-
-            Countdown.Text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}", timeLeft.Days, timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
-
+            if (DateTime.Now >= launch)
+            {
+                textBlock1.Text = "";
+                textBlock2.Text = "";
+                textBlock3.Text = "";
+                textBlock4.Text = "";
+                textblockRelease.Text = "";
+                Countdown.Height = 70;
+                Countdown.FontSize = 34;
+                Countdown.Text = "Diablo 3 is released";
+            }
+            else
+            {
+                Countdown.Text = string.Format("{0:D2}:{1:D2}:{2:D2}:{3:D2}", timeLeft.Days, timeLeft.Hours, timeLeft.Minutes, timeLeft.Seconds);
+            }
         }
     }
 }
